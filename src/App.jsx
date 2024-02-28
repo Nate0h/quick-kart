6d47afe8e8e892db6e4c5553c80cb9564f2dbe83
Nathaniel Colon <nathanielcolon12@gmail.com>
2024-02-28 18:44:52 -0500

    Removed styling from navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Shop from "./components/Shop";
