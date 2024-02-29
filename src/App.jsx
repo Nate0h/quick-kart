3ba59b220fa5bc256cb8f6dacfbcf575fad8dad9
Nathaniel Colon <nathanielcolon12@gmail.com>
2024-02-28 21:14:34 -0500

    Added fetch for Fake Store API

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import axios from "axios";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Shop from "./components/Shop";
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,5 +1,33 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 const Shop = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+        return response.json();
+      })
+      .then((actualData) => {
+        setData(actualData);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setData(null);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div>
       <div className="banner">Banner</div>
